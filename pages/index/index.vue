dfb810f0bf06a42cf63b5f72e71d531a378bdf9d
程晓峰 <xiaofeng.cheng@macrowing.com>
2021-08-26 15:17:08 +0800

    修改图标

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,10 +2,18 @@
 	<view class="container">
 		<view class="status_bar">
 			<!-- 这里是状态栏 -->
-			<view class="left-icon" />
-			<u-search placeholder="自动获得焦点" v-model="value" input-style="{width: 70px}" :show-action="false"
-				bg-color="#fff" @search="search"/>
-			<view class="right-btn" type="default">
+			<view class="left-icon">
+				<u-circle-progress width="70" border-width="6" active-color="#e68f25" :percent="percent">
+					<view class="u-progress-content">
+						<u-image width="51rpx" height="51rpx" src="../../imgs/red-bag.png"></u-image>
+					</view>
+				</u-circle-progress>
+			</view>
+			<view class="search">
+				<u-search placeholder="自动获得焦点" v-model="value" :input-style="{width:'70px'}" :show-action="false"
+					bg-color="#fff" @search="search" />
+			</view>
+			<view class="right-btn" type="default" @tap="openModal">
 				<view class="circle">
 					<u-icon name="plus" color="#d33d3e" size="12"></u-icon>
 				</view>
@@ -34,7 +42,8 @@
 		data() {
 			return {
 				news: [],
-				value: ''
+				value: '',
+				percent: 0
 			};
 		},
 		onLoad: function() {
@@ -42,6 +51,12 @@
 				title: "加载中...."
 			})
 			this.getData()
+			this.circleInterval = setInterval(() => {
+				if(this.percent == 100) {
+					this.percent = 0
+				}
+				this.percent += 1
+			}, 100)
 		},
 		methods: {
 			getData(params) {
@@ -65,7 +80,10 @@
 				});
 			},
 			search(e) {
-				
+
+			},
+			openModal() {
+
 			}
 		},
 	}
@@ -86,22 +104,38 @@
 			align-items: center;
 			background-color: #d33d3e;
 
+			.search {
+				width: 75%;
+			}
+
 			.left-icon {
 				height: 35px;
 				width: 35px;
 				border-radius: 50%;
 				background-color: #fff;
+				align-items: center;
+				justify-content: center;
+				.u-circle-progress {
+					border-radius: 50%;
+					.u-progress-content {
+						display: flex;
+						align-items: center;
+						justify-content: center;
+					}
+				}
+				
 			}
 
 			.right-btn {
 				height: 35px;
-				width: 10%;
+				width: 7%;
 				display: flex;
 				flex-direction: column;
+				align-items: center;
 
 				.circle {
-					width: 50%;
-					height: 60%;
+					width: 70%;
+					height: 100%;
 					border-radius: 50%;
 					background-color: #fff;
 					color: #d33d3e;
