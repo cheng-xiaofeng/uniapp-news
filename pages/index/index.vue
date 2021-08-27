138122e10065c2299f7b231f1bf4b1a9b6dc82ff
程晓峰 <xiaofeng.cheng@macrowing.com>
2021-08-27 15:27:28 +0800

    添加弹窗

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -13,7 +13,7 @@
 				<u-search placeholder="自动获得焦点" v-model="value" :input-style="{width:'70px'}" :show-action="false"
 					bg-color="#fff" @search="search" />
 			</view>
-			<view class="right-btn" type="default" @tap="openModal">
+			<view class="right-btn" type="default" @tap="togglePopup(true)">
 				<view class="circle">
 					<u-icon name="plus" color="#d33d3e" size="12"></u-icon>
 				</view>
@@ -34,6 +34,53 @@
 				</view>
 			</view>
 		</view>
+		<view>
+			<u-popup v-model="showPopup" mode="bottom" class="popup">
+				<view class="userInfo">
+					<view>
+						<u-avatar :style="{marginRight: '20rpx'}" @tap="goto" src="../../imgs/avatar.jpg" size="51">
+						</u-avatar>
+						<text class="words">{{userName}}</text>
+					</view>
+					<text class="words">草稿箱({{draftNumber}})</text>
+				</view>
+				<view class="tabs">
+					<view class="tabs-list" v-for="(item,index) in tabList" :key="index" @tap="goto(item.url)">
+						<u-image width="51rpx" height="51rpx" :src="item.img"></u-image>
+						<text class="words">{{item.name}}</text>
+					</view>
+				</view>
+				<view class="suggestion">
+					<view class="container">
+						<view class="suggest">
+							<u-image width="32rpx" height="32rpx" src="../../static/bottomModal/crown.png"></u-image>
+							<text class="words">加入创作者计划,发布作品赚现金</text>
+							<u-icon name="arrow-right"></u-icon>
+						</view>
+						<u-icon name="close" size="20"></u-icon>
+					</view>
+				</view>
+				<view class="creation">
+					<view class="create">
+						<text class="words">创作热点</text>
+						<view class="right-info">
+							<text class="sub-title">{{titleA}}</text>
+							<u-icon name="arrow-right"></u-icon>
+						</view>
+					</view>
+					<view class="create">
+						<text class="words">创作活动</text>
+						<view class="right-info">
+							<text class="sub-title">{{titleB}}</text>
+							<u-icon name="arrow-right"></u-icon>
+						</view>
+					</view>
+				</view>
+				<view class="closeBtn">
+					<u-icon name="close" size="28" @tap="togglePopup(false)"></u-icon>
+				</view>
+			</u-popup>
+		</view>
 	</view>
 </template>
 
@@ -43,7 +90,29 @@
 			return {
 				news: [],
 				value: '',
-				percent: 0
+				percent: 0,
+				showPopup: false,
+				draftNumber: 0,
+				userName: '用户名',
+				tabList: [{
+					img: '../../static/bottomModal/message.png',
+					name: '微头条',
+					url: ''
+				}, {
+					img: '../../static/bottomModal/article.png',
+					name: '文章',
+					url: ''
+				}, {
+					img: '../../static/bottomModal/q-a.png',
+					name: '问答',
+					url: ''
+				}, {
+					img: '../../static/bottomModal/video.png',
+					name: '拍小视频',
+					url: ''
+				}],
+				titleA: '#起底周江勇罗马按实际发动机撒放假啊司法局',
+				titleB: '参与活动,赢取奖励'
 			};
 		},
 		onLoad: function() {
@@ -52,11 +121,11 @@
 			})
 			this.getData()
 			this.circleInterval = setInterval(() => {
-				if(this.percent == 100) {
+				if (this.percent == 100) {
 					this.percent = 0
 				}
 				this.percent += 1
-			}, 100)
+			}, 500)
 		},
 		methods: {
 			getData(params) {
@@ -82,8 +151,13 @@
 			search(e) {
 
 			},
-			openModal() {
-
+			togglePopup(bool) {
+				this.showPopup = bool
+			},
+			goto(url) {
+				uni.navigateTo({
+					url
+				});
 			}
 		},
 	}
@@ -115,15 +189,11 @@
 				background-color: #fff;
 				align-items: center;
 				justify-content: center;
+
 				.u-circle-progress {
 					border-radius: 50%;
-					.u-progress-content {
-						display: flex;
-						align-items: center;
-						justify-content: center;
-					}
 				}
-				
+
 			}
 
 			.right-btn {
@@ -162,6 +232,86 @@
 			.uni-media-list-text-top {
 				line-height: 1.6em;
 			}
+		}
+
+
+	}
+
+	/deep/.popup .u-drawer-content {
+		padding: 10px 30px 0 30px;
+		border-top-left-radius: 5px;
+		border-top-right-radius: 5px;
+
+		.words {
+			font-weight: bold;
+			font-family: 'qingyuan';
+		}
+
+		.userInfo {
+			height: 50px;
+			justify-content: space-between;
+		}
+
+		.tabs {
+			justify-content: space-between;
+
+			.tabs-list {
+				width: 25%;
+				justify-content: center;
+				align-items: center;
+				flex-direction: column;
+			}
+		}
+
+		.suggestion {
+			height: 90px;
+			align-items: center;
+
+			.container {
+				height: 50%;
+				width: 100%;
+				padding-left: 20px;
+				padding-right: 20px;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				background-color: #fdfbee;
+				border-radius: 5px;
+
+				.suggest {
+					width: 80%;
+				}
+			}
+		}
+
+		.creation {
+			display: block;
+			width: 100%;
+
+			.create {
+				width: 100%;
+				height: 50px;
+				display: block;
+				.right-info {
+					width: 50%;
+					float: right;
+					color: rgb(144, 147, 153);
+
+					.sub-title {
+						width: 90%;
+						text-align: right;
+						overflow: hidden;
+						text-overflow: ellipsis;
+						white-space: nowrap;
+						margin-right: 5px;
+					}
+				}
+			}
+		}
+
+		.closeBtn {
+			justify-content: center;
+			margin-bottom: 30px;
 		}
 	}
 </style>
