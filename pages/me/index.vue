f726bb16358dd6fdff993921c08e9f347c83056c
cheng-xiaofeng <771185591@qq.com>
2021-09-03 13:44:30 +0800

    添加图片

--- pages/me/index.vue
+++ pages/me/index.vue
@@ -1,26 +1,34 @@
 <template>
-	<view>
+	<view class="container">
+		<me-info />
 		<other />
 	</view>
 </template>
 <script>
+	import MeInfo from './me-info/index.vue'
 	import Other from './others/index.vue'
 	export default {
-		components:{
+		components: {
+			MeInfo,
 			Other
 		},
 		mounted() {
-			
+
 		},
-		methods:{
-			
+		methods: {
+
 		}
 	}
 </script>
 <style lang="less">
+	.container {
+		display: block;
+		padding-left: 20px;
+	}
+
 	.grid-text {
-			font-size: 28rpx;
-			margin-top: 4rpx;
-			color: #232323;
-		}
+		font-size: 28rpx;
+		margin-top: 4rpx;
+		color: #232323;
+	}
 </style>
--- pages/me/me-info/index.vue
+++ pages/me/me-info/index.vue
@@ -1,11 +1,31 @@
 <template>
 	<view>
-		
+		<view class="userInfo">
+			<view>
+				<u-avatar :style="{marginRight: '20rpx'}" @tap="goto" src="../../../static/avatar.jpg" size="100">
+				</u-avatar>
+				<h3 class="words">{{userName}}</h3>
+				<u-icon name="arrow-right"  size="28" :custom-style ="{marginLeft: '6px'}"></u-icon>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				userName: '不玩游戏的程序员'
+			}
+		}
+	}
 </script>
 
-<style>
+<style lang="less">
+	.userInfo {
+		margin-top: 20px;
+		h3 {
+			line-height: 55px;
+		}
+	}
 </style>
